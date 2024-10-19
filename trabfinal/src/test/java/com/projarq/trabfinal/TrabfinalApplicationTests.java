e4ad860c274936b2817055375bde36d335b6ed41
gustavo <gustavobpasquali2@gmail.com>
2024-10-19 16:43:38 -0300

    show de bola

---
+++ trabfinal/src/test/java/com/projarq/trabfinal/TrabfinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projarq.trabfinal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabfinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
